8674d8fec3b8d64042f3a49b65af9386101c4bbd
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-02-13 21:00:00 -0500

    Added save and load functionality for previous search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,36 @@ const apiKey = '18383a477dce3195768091ae9235b252';
 
 $(function() {
     
+    // After DOM initializes, create buttons for each saved search option
+    for(var location of loadLocations()){
+        $('<li />')
+        .append($('<button />', {
+            text: location.name
+        }))
+        .appendTo('#saved-searchs');
+    }
+    
+    // Function to load and return previously searched locations
+    function loadLocations(){
+        var savedLocations = JSON.parse(localStorage.getItem('savedLocations'));
+        if(!savedLocations){
+            savedLocations = [];
+        }
+        return savedLocations;
+    }
+    
+    // Function to save a new location to previous searchs
+    function saveLocation(locationData){
+        var locations = loadLocations();
+
+        // Don't save if it already exists
+        if(!locations.some(i => i.name === locationData.name)){
+            locations.push(locationData);
+        }
+        localStorage.setItem('savedLocations',JSON.stringify(locations));
+    }
+
+    // Function to query openWeather for weather data at a location
     function forecastLookup(lat, lon){
         fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&cnt=6`)
         .then(function (response){
@@ -12,6 +42,7 @@ $(function() {
         })
     }
 
+    // Function to query openWeather geocoder api for locations data of a city
     function locationLookup(location){
         fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`)
         .then(function (response){
